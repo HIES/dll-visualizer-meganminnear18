f476c519c9000eb0e7d508f3c8e7d6a95bcaf695
meganminnear18 <31036480+meganminnear18@users.noreply.github.com>
2017-10-12 20:15:26 -0400

    update1

--- DLLVisualizer.java
+++ DLLVisualizer.java
@@ -6,6 +6,7 @@ import bridges.base.DLelement;
  *
  * @author (your name)
  */
+
 public class DLLVisualizer
 {
     /**
@@ -15,19 +16,37 @@ public class DLLVisualizer
     public static void main(String[] args) throws Exception{
 
         // TO DO: Define credentials
-        final String API_KEY = "INSERT_API_KEY_HERE";
-        final String USER_ID = "INSERT_USER_ID_HERE";
+        final String API_KEY = "891238522444";
+        final String USER_ID = "meganminnear18";
         
         // No need to modify Bridges constructor
         Bridges bridge = new Bridges(1, API_KEY, USER_ID);
 
         // TO DO: Build the objects to be added to your DLL
-        
+        Track song1 = new Track("Mr. Blue Sky", "Electric Light Orchestra");
+        Track song2 = new Track("Don't Bring Me Down", "Electric Light Orchestra");
+        Track song3 = new Track("Uncle John's Band", "Grateful Dead");
+        Track song4 = new Track("Ripple", "Grateful Dead");
+
 
         // TO DO: Build the head 
-        DLelement head = null;
+        DLelement head = new DLelement(song1.toString(), song1);
         
         // TO DO: Create the rest of the Nodes/DLelements and link them to form a DLL
+        DLelement next1 = new DLelement(song2.toString(), song2);
+        DLelement next2 = new DLelement(song3.toString(), song3);
+        DLelement next3 = new DLelement(song4.toString(), song4);
+        
+
+        head.setNext(next1);
+        head.setPrev(next3);
+        next1.setNext(next2);
+        next1.setPrev(head);
+        next2.setNext(next3);
+        next2.setPrev(next2);
+        next3.setNext(head);
+        next3.setPrev(next2);
+        
         
         //Prepare for visualization
         bridge.setDataStructure(head);
